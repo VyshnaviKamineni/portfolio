329c1c233bec6f927af962b81c627ef76dea56cf
VyshnaviKamineni <vyshnavidevipriya@gmail.com>
2024-05-29 20:58:22 +0530

    Updates

---
+++ precache-manifest.ca942b367209c0dd4bdef5ddc5a33240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce669f4555fdff692bc0313c02eccc61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c5491ea547c909cf9243",
+    "url": "/portfolio/static/css/main.af2e5133.chunk.css"
+  },
+  {
+    "revision": "55eafab07f3bf6100969",
+    "url": "/portfolio/static/js/2.446163e9.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/portfolio/static/js/2.446163e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5491ea547c909cf9243",
+    "url": "/portfolio/static/js/main.bd711749.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
